360961a3311384c559b27644ab4bab1adf2ae97c
sunil sapkota <sunil421sapkota@gmail.com>
2025-01-03 20:09:39 +0545

    educations responsive

--- src/Componenets/Education.jsx
+++ src/Componenets/Education.jsx
@@ -3,14 +3,18 @@ import React from "react";
 const Education = (props) => {
   return (
     <>
-      <div className="w-[80%]  max-h-fit  mt-3 p-5 rounded-lg  border-l-4 border-solid border-primaryColor bg-secondaryColor">
-        <div className=" flex bg-secondaryColor justify-between items-center min-w-full px-4">
-          <div className="text-xl bg-secondaryColor">{props.academic_name}</div>
+      <div className=" w-[95%]   lg:w-[80%]  max-h-fit  mt-3 p-5 rounded-lg  border-l-4 border-solid  border-primaryColor bg-secondaryColor">
+        <div className=" flex bg-secondaryColor justify-between items-center min-w-full px-2  lg:px-4">
+          <div className="text-xl text-primaryColor  bg-secondaryColor">
+            {props.academic_name}
+          </div>
           <div className="text-zinc-500 text-base bg-secondaryColor">
             {props.duration}
           </div>
         </div>
-        <h1 className="bg-secondaryColor px-4">{props.classes}</h1>
+        <h1 className="bg-secondaryColor px-2 font-logoFont lg:px-4 ">
+          {props.classes}
+        </h1>
       </div>
     </>
   );
