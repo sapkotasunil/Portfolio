93984bce5fc2d9661f4dd5b91ccf5ecfdfa0c8e5
sunil sapkota <sunil421sapkota@gmail.com>
2025-01-05 22:25:20 +0545

    sunday1

--- src/Componenets/Education.jsx
+++ src/Componenets/Education.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Education = (props) => {
   return (
     <>
-      <div className=" w-[99%]   lg:w-[80%]  max-h-fit  mt-3 p-1 py-3 lg:p-5 rounded-lg  border-l-4 border-solid  border-primaryColor bg-secondaryColor">
+      <div className=" w-[99%]   lg:w-[80%]  max-h-fit  mt-3 p-1 py-3 lg:p-5 rounded-lg  border-l-4 border-solid  border-blue-700 bg-secondaryColor">
         <div className=" flex bg-secondaryColor justify-between text items-center min-w-full pl-2  lg:px-4">
           <div className="text-xl text-primaryColor  bg-secondaryColor">
             {props.academic_name}
--- src/Componenets/Skills.jsx
+++ src/Componenets/Skills.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Skills = (props) => {
   return (
-    <div className="max-h-[16vh] min-w-full lg:min-w-[40vw] border-l-4 border-solid flex-wrap  border-primaryColor  rounded-lg flex px-1 py-2 mt-4  gap items-center ml-1 bg-secondaryColor">
+    <div className="max-h-[16vh] min-w-full lg:min-w-[40vw] border-l-4 border-solid flex-wrap  border-blue-700  rounded-lg flex px-1 py-2 mt-4  gap items-center ml-1 bg-secondaryColor">
       <div className="max-h-fit w-36  bg-secondaryColor ">{props.icon}</div>
       <div className=" bg-secondaryColor text-primaryColor text-3xl ">
         {props.skillName}
